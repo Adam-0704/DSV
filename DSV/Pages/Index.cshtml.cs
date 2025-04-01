35767031e010769301c627fb4c3680e4a96671a3
Adam-0704 <adamchino8@gmail.com>
2025-04-01 08:54:09 +0100

    Add project files.

---
+++ DSV/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+using DSV.classes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DSV.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<MeetingRoom> MeetingRooms;
+
+        public void OnGet()
+        {
+            MeetingRooms = new List<MeetingRoom>
+        {
+            new MeetingRoom { ID = 1, Name = "Mødelokale A", Capacity = 10, IsAvailable = true },
+            new MeetingRoom { ID = 2, Name = "Mødelokale B", Capacity = 20, IsAvailable = false },
+            new MeetingRoom { ID = 3, Name = "Mødelokale C", Capacity = 15, IsAvailable = true }
+        };
+
+        }
+       
+
+    }
+}
---
+++ DSV/classes/MeetingRoom.cs
@@ -0,0 +1,11 @@
+﻿namespace DSV.classes
+{
+    public class MeetingRoom
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public bool IsAvailable { get; set; }
+
+    }
+}
