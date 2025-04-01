b7cd7018b9ce70b68e00478453f602fef15c878b
Adam-0704 <adamchino8@gmail.com>
2025-04-01 16:49:59 +0200

    jeg tilføjet en knap, med link, og fik fjernet en navigation link.

---
+++ DSV/Pages/BookingPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DSV.Pages
+{
+    public class BookingPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
