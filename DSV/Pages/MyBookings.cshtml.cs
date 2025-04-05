38f2c6a5d454dc4dbcbdac990e03dea7a2ca2974
Patrick-LD <patricklykkedahl@gmail.com>
2025-04-05 14:19:07 +0200

    menupunkt til mybookings, glemte booking model

---
+++ DSV/Pages/MyBookings.cshtml.cs
@@ -0,0 +1,25 @@
+using DSV.classes;
+using DSV.Services;
+using DSV.classes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DSV.Pages
+{
+    public class MyBookingsModel : PageModel
+    {
+        private readonly IBookingService _bookingService;
+
+        public List<Booking> MyBookings { get; private set; }
+
+        public MyBookingsModel(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+
+        public void OnGet()
+        {
+            MyBookings = _bookingService.GetBookings();
+        }
+    }
+}
---
+++ DSV/classes/Booking.cs
@@ -0,0 +1,12 @@
+﻿namespace DSV.classes
+{
+    public class Booking
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public DateOnly DateOnly { get; set; }
+        public TimeOnly TimeOnly { get; set; }
+        public int RoomID { get; set; } // RoomID property to link to a room
+    }
+
+}
